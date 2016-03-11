aab227f39d1d771dddb7affb3e33be405b4930a9
katebee <whalenka88@gmail.com>
2016-03-11 17:03:40 +0000

    Basic python version done!

--- snakes_and_gems.py
+++ snakes_and_gems.py
@@ -25,20 +25,27 @@ class SnakesAndGems(object):
         self.boxes.remove('snakes')
         print self.boxes # see all!
 
-    def offer_to_swap(self, player):
-        if player.swap_or_not():
+    def offer_to_swap(self, player, prompt):
+        if player.swap_or_not(prompt):
             player.box = self.boxes[0]
             print 'The boxes have been swapped!'
         else:
             print 'You will keep your first choice then!'
+
+    def win_lose(self, player):
+        if player.box == 'snakes':
+            print "Oh no! you open your box and find snakes!"
+        else:
+            print "Hooray! You recieved a box of gems!"
 
     def run_game(self, player):
         self.game_intro()
         player.choose_box('The boxes have been shuffled, which one will you pick? (1,2,3) >> ')
         self.remove_a_box(player)
         print 'I have removed one of the other boxes... it was full of snakes'
-        print 'Would you like to swap you box with the remaining box? (y / N)'
-        self.offer_to_swap(player)
+        self.offer_to_swap(player, 'Would you like to swap you box with the remaining box? (y / N) >> ')
+        self.wait_and_space(0.5)
+        self.win_lose(player)
 
 
 class Player(object):
@@ -59,8 +66,8 @@ class Player(object):
             except ValueError:
                 print "That is not a number!"
 
-    def swap_or_not(self):
-        player_answer = raw_input().upper()
+    def swap_or_not(self, prompt):
+        player_answer = raw_input(prompt).upper()
         return True if player_answer[:1] == 'Y' else False
 
 

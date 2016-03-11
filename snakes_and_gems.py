1fa9a312ee986bb1c923cb24f68b6deed083c66c
katebee <whalenka88@gmail.com>
2016-03-11 14:41:36 +0000

    Add Player class and responsibilities

--- snakes_and_gems.py
+++ snakes_and_gems.py
@@ -5,7 +5,6 @@ class SnakesAndGems(object):
     """docstring for SnakesAndGems"""
     def __init__(self):
         self.boxes = ['gems', 'snakes', 'snakes']
-        self.player = None
         self.player_choice = None
     # set up the game
     # get user input_check
@@ -17,24 +16,42 @@ class SnakesAndGems(object):
         print 'Welcome to Gems and Snakes!'
         self.wait_and_space(0.5)
         print 'I have three boxes, one contains gems, the others are full of snakes:'
-        print '|Snakes| |Snakes| |Gems|'
+        print self.boxes
         self.wait_and_space(0.5)
         random.shuffle(self.boxes)
-        print 'The boxes have been shuffled, which one will you pick? (1,2,3)'
 
-    def run_game(self):
+    def remove_a_box(self, player):
+        player.box = self.boxes.pop(player.choice - 1)
+        self.boxes.remove('snakes')
+        print self.boxes # see all!
+
+    def run_game(self, player):
         self.game_intro()
+        player.choose_box('The boxes have been shuffled, which one will you pick? (1,2,3) >> ')
+        self.remove_a_box(player)
 
 class Player(object):
     """docstring for Player"""
-    def __init__(self, arg):
-        super(Player, self).__init__()
-        self.arg = arg
+    def __init__(self):
+        self.box = None
+        self.choice = None
 
+    def choose_box(self, prompt):
+        while True:
+            try:
+                player_input = int(raw_input(prompt))
+                if 1 <= player_input <= len(game.boxes):
+                    self.choice = player_input
+                    return
+                else:
+                    self.choose_box(prompt)
+            except ValueError:
+                print "That is not a number!"
 
 # ############ GAME START ############ #
 if __name__ == '__main__':
     # create a new game
 
     game = SnakesAndGems()
-    game.run_game()
+    player = Player()
+    game.run_game(player)

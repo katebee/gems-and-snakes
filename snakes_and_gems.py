56ce6e25f9f6cb3fe40a0a559ae48a53cfdd3ac9
katebee <whalenka88@gmail.com>
2016-03-11 12:57:09 +0000

    Initial python version

---
+++ snakes_and_gems.py
@@ -0,0 +1,18 @@
+
+class SnakesAndGems(object):
+
+    def __init__(self):
+        self.boxes = ['gems', 'snakes', 'snakes']
+    # set up the game
+    # get user input_check
+
+    def run_game(self):
+        print "running"
+
+
+# ############ GAME START ############ #
+if __name__ == "__main__":
+    # create a new game
+
+    game = SnakesAndGems()
+    game.run_game()

5b5c3b1c8dcaf018a26227f1e14c311ddcb9f953
katebee <whalenka88@gmail.com>
2016-03-15 15:08:05 +0000

    Add basic Player and tests

--- scrabble/scrabble.py
+++ scrabble/scrabble.py
@@ -1,10 +1,17 @@
 
+import random
+
+
 class Game(object):
     """docstring for Game"""
     def __init__(self):
         self.letter_values = {'E': 1, 'D': 2, 'B': 3}
         self.letter_distribution = {'E': 12, 'D': 4, 'B': 2}
-        self.scrabble_bag = self.generate_scrabble_bag()
+        self.scrabble_bag = self._generate_scrabble_bag()
+
+    def run(self):
+        player = Player(self._generate_rack())
+        print player.rack
 
     def word_score(self, word):
         """Calculate the score for a word"""
@@ -13,17 +20,29 @@ class Game(object):
             score += self.letter_values[letter]
         return score
 
-    def generate_scrabble_bag(self):
-        """Letter distribution is based on the English Scrabble distribution"""
+    def _generate_scrabble_bag(self):
+        """Returns a shuffled list of letters. Letter distribution is based on
+        the English Scrabble distribution"""
         scrabble_bag = []
         for letter in self.letter_distribution:
             for _ in range(self.letter_distribution[letter]):
                 scrabble_bag.append(letter)
+        random.shuffle(scrabble_bag)
         return scrabble_bag
 
-    def random_tile(self):
-        """Assign seven tiles chosen randomly from the alphabet"""
-        pass
+    def _generate_rack(self):
+        """Assign seven tiles from a scrabble_bag"""
+        rack = []
+        for _ in range(7):
+            rack.append(self.scrabble_bag.pop())
+        rack.sort()
+        return rack
+
+
+class Player(object):
+    """docstring for Player"""
+    def __init__(self, rack):
+        self.rack = rack
 
 
 # ########### GAME START ########### #
@@ -33,6 +52,7 @@ if __name__ == '__main__':
     print 'Running Scrabble'
 
     game = Game()
+    game.run()
 
     print '\'BEE\' is worth %d points' % game.word_score('BEE')
     print '\'BED\' is worth %d points' % game.word_score('BED')
--- scrabble/tests.py
+++ scrabble/tests.py
@@ -1,7 +1,8 @@
 
 import unittest
 
-from scrabble import Game
+from scrabble import Game, Player
+
 
 class TestGame(unittest.TestCase):
 
@@ -19,6 +20,22 @@ class TestGame(unittest.TestCase):
 
     def test_scrabble_bag(self):
         self.assertEqual(len(self.game.scrabble_bag), 18)
+
+    def test_generate_rack(self):
+        rack = self.game._generate_rack()
+        self.assertEqual(len(rack), 7)
+        self.assertEqual(len(self.game.scrabble_bag), 11)
+
+
+class TestPlayer(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        self.game = Game()
+        self.player = Player(['E', 'D', 'B'])
+
+    def test_player_rack(self):
+        self.assertEqual(self.player.rack, ['E', 'D', 'B'])
 
 
 if __name__ == '__main__':

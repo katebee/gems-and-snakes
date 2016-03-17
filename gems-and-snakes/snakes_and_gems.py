301979f4de9a1a033c5c5a0e6c3c1a5cfe3ce942
katebee <whalenka88@gmail.com>
2016-03-17 11:40:45 +0000

    Record stats for each game

--- gems-and-snakes/snakes_and_gems.py
+++ gems-and-snakes/snakes_and_gems.py
@@ -1,5 +1,8 @@
 import random
 import time
+import json
+import sys
+# could also use sys argsv ArgumentParser()
 
 class SnakesAndGems(object):
     """docstring for SnakesAndGems"""
@@ -16,14 +19,15 @@ class SnakesAndGems(object):
         print 'Welcome to Gems and Snakes!'
         self.wait_and_space(0.5)
         print 'I have three boxes, one contains gems, the others are full of snakes:'
-        print self.boxes
+        print self.boxes  # format this nicely... unicode icons?
         self.wait_and_space(0.5)
         random.shuffle(self.boxes)
 
     def remove_a_box(self, player):
         player.box = self.boxes.pop(player.choice - 1)
         self.boxes.remove('snakes')
-        print self.boxes # see all!
+        print 'I have removed one of the other boxes... it was full of snakes'
+        # print self.boxes # see all!
 
     def offer_to_swap(self, player, prompt):
         if player.swap_or_not(prompt):
@@ -37,22 +41,52 @@ class SnakesAndGems(object):
             print "Oh no! you open your box and find snakes!"
         else:
             print "Hooray! You recieved a box of gems!"
+            player.won = True
 
     def run_game(self, player):
         self.game_intro()
         player.choose_box('The boxes have been shuffled, which one will you pick? (1,2,3) >> ')
         self.remove_a_box(player)
-        print 'I have removed one of the other boxes... it was full of snakes'
         self.offer_to_swap(player, 'Would you like to swap you box with the remaining box? (y / N) >> ')
         self.wait_and_space(0.5)
         self.win_lose(player)
+        stats = self.record_stats_context_manager(player)
+        self.print_stats(stats)
 
+# def record_stats(win_or_not):
+#     stats_file = open("stats.json", "r+")
+#     print stats_file.read()
+#     stats_file.close()
+
+    def record_stats_context_manager(self, player):
+        # split reading and storing stats.
+        with open("stats.json", "r+") as stats_file:
+            # print stats_file.read()
+            stats = json.load(stats_file)
+            # put below in own function, no longer seek, just open and overwrite
+            stats_file.seek(0)
+            stats['play_count'] += 1
+            if player.won:
+                stats["won_count"] += 1
+                if player.swap:
+                    stats["swapped_and_won"] += 1
+            else:
+                if player.swap:
+                    stats["swapped_and_lost"] += 1
+            json.dump(stats, stats_file)
+            return stats
+
+    def print_stats(self, stats):
+        if len(sys.argv) > 1:
+            print stats
 
 class Player(object):
     """docstring for Player"""
     def __init__(self):
         self.box = None
         self.choice = None
+        self.swap = False
+        self.won = False
 
     def choose_box(self, prompt):
         while True:
@@ -68,7 +102,11 @@ class Player(object):
 
     def swap_or_not(self, prompt):
         player_answer = raw_input(prompt).upper()
-        return True if player_answer[:1] == 'Y' else False
+        if player_answer[:1] == 'Y':
+            self.swap = True
+            return True
+        else:
+            return False
 
 
 

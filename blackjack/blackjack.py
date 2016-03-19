667ac93d696e09842bdf27352d5b3b0b39da6a9e
katebee <whalenka88@gmail.com>
2016-03-19 15:41:55 +0000

    Can deal and evaluate blackjack hand

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -17,6 +17,35 @@ class Game(object):
         random.shuffle(card_deck)
         return card_deck
 
+    def deal_hand(self):
+        hand = []
+        hand.append(self.card_deck.pop())
+        hand.append(self.card_deck.pop())
+        return hand
+
+    def evaluate_card(self, card):
+        value = card[:-1]
+        if value == 'J' or value == 'Q' or value == 'K':
+            return 10
+        elif value == 'A':
+            return 11
+        else:
+            return int(value)
+
+    def evaluate_hand(self, hand):
+        hand_total = 0
+        for card in hand:
+            hand_total += self.evaluate_card(card)
+        return hand_total
+
+
+class Player(object):
+    """docstring for Player"""
+    def __init__(self, name, hand):
+        self.name = name
+        self.hand = hand
+
+
 # ############ GAME START ############ #
 
 if __name__ == '__main__':
--- blackjack/tests.py
+++ blackjack/tests.py
@@ -13,5 +13,23 @@ class TestGame(unittest.TestCase):
     def test_generate_deck(self):
         self.assertEqual(len(self.game.card_deck), 52)
 
+    def test_deal_hand(self):
+        hand = self.game.deal_hand()
+        self.assertEqual(len(hand), 2)
+        self.assertEqual(len(self.game.card_deck), 50)
+
+    def test_evaluate_card(self):
+        self.assertEqual(self.game.evaluate_card('JC'), 10)
+        self.assertEqual(self.game.evaluate_card('QH'), 10)
+        self.assertEqual(self.game.evaluate_card('KS'), 10)
+        self.assertEqual(self.game.evaluate_card('AD'), 11)
+
+    def test_evaluate_hand(self):
+        self.assertEqual(self.game.evaluate_hand(['KS', 'AD']), 21)
+        self.assertEqual(self.game.evaluate_hand(['7C', '8H']), 15)
+        self.assertEqual(self.game.evaluate_hand(['4S', '10C']), 14)
+        self.assertEqual(self.game.evaluate_hand(['QH', '10D']), 20)
+
+
 if __name__ == '__main__':
     unittest.main()

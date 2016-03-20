86602b241887f5bfdb491102ed438cb6bf4f873c
katebee <whalenka88@gmail.com>
2016-03-20 21:38:55 +0000

    Player can draw a card and display current hand

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -38,6 +38,11 @@ class Game(object):
             hand_total += self.evaluate_card(card)
         return hand_total
 
+    def draw_a_card(self):
+        if len(self.card_deck) > 0:
+            card = self.card_deck.pop()
+            return card
+
 
 class Player(object):
     """docstring for Player"""
@@ -45,9 +50,27 @@ class Player(object):
         self.name = name
         self.hand = hand
 
+    def draw_a_card(self, card_deck):
+        if len(card_deck) > 0:
+            card = card_deck.pop()
+            self.hand.append(card)
+            return card
+
+    def display_hand(self):
+        return ' '.join(self.hand)
+
 
 # ############ GAME START ############ #
 
 if __name__ == '__main__':
 
     print 'Hello, Blackjack'
+
+    game = Game()
+    player = Player('Player', game.deal_hand())
+    dealer = Player('Dealer', game.deal_hand())
+
+    for _ in range(10):
+        player.draw_a_card(game.card_deck)
+
+    print player.display_hand()
--- blackjack/tests.py
+++ blackjack/tests.py
@@ -1,7 +1,7 @@
 
 import unittest
 
-from blackjack import Game
+from blackjack import Game, Player
 
 
 class TestGame(unittest.TestCase):
@@ -29,6 +29,25 @@ class TestGame(unittest.TestCase):
         self.assertEqual(self.game.evaluate_hand(['7C', '8H']), 15)
         self.assertEqual(self.game.evaluate_hand(['4S', '10C']), 14)
         self.assertEqual(self.game.evaluate_hand(['QH', '10D']), 20)
+
+
+class TestPlayer(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        self.player = Player('Player', ['7C', '8H'])
+        self.dealer = Player('Dealer', ['KS', 'AD'])
+
+    def test_display_hand(self):
+        self.assertEqual(self.player.display_hand(), '7C 8H')
+        self.assertEqual(self.dealer.display_hand(), 'KS AD')
+
+    def test_draw_a_card(self):
+        deck = ['5S', '2D', '3C']
+        self.assertEqual(self.player.draw_a_card(deck), '3C')
+        self.assertEqual(len(self.dealer.hand), 2)
+        self.assertEqual(len(self.player.hand), 3)
+        self.assertEqual(len(deck), 2)
 
 
 if __name__ == '__main__':

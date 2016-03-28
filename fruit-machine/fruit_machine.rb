4b723af766e29af40dbfce23b5aa7d3d5fb73ea8
katebee <whalenka88@gmail.com>
2016-03-28 19:08:05 +0100

    Add win condition checks

--- fruit-machine/fruit_machine.rb
+++ fruit-machine/fruit_machine.rb
@@ -12,11 +12,27 @@ class Machine
     colours = ['black', 'white', 'green', 'yellow']
     [colours.sample, colours.sample, colours.sample, colours.sample]
   end
+
+  def unique_set_win?(slots)
+    slots.uniq.length == slots.length ? true : false
+  end
+
+  def adjacent_win?(slots)
+    slots.each_with_index do |slot, index|
+      next_slot = slots[index + 1]
+      if next_slot.nil?
+        return false
+      elsif slot == next_slot
+        return true
+      end
+    end
+  end
 end
 
 # ########### GAME START ########### #
 
 if __FILE__ == $PROGRAM_NAME
   machine = Machine.new(222, 22)
-  puts machine.roll_slots
+  roll = machine.roll_slots
+  puts "#{roll}"
 end

b52b09a19f750e643d663f73ec9b477744df34ce
katebee <whalenka88@gmail.com>
2016-03-29 23:55:01 +0100

    Able to determine prize amount

--- fruit-machine/fruit_machine.rb
+++ fruit-machine/fruit_machine.rb
@@ -16,17 +16,20 @@ class Player
 end
 
 class Machine
-  attr_reader :slots, :play_cost
+  attr_reader :slots, :play_cost, :play_credit
   attr_accessor :machine_bank
 
   def initialize(machine_bank, play_cost)
     @machine_bank = machine_bank
     @play_cost = play_cost
+    @play_credit = 0
   end
 
   def run
     roll = roll_slots
     puts roll.to_s
+    prize = determine_prize(roll)
+    puts "WINNINGS: #{prize}"
   end
 
   def roll_slots
@@ -48,12 +51,31 @@ class Machine
       end
     end
   end
+
+  def determine_prize(result)
+    if unique_set_win?(result)
+      prize = @machine_bank / 2
+      @machine_bank -= prize
+      return prize
+    elsif adjacent_win?(result)
+      if @machine_bank < @play_cost * 5
+        prize = @machine_bank
+        @machine_bank = 0
+        return prize
+      else
+        @machine_bank -= @play_cost * 5
+        return @play_cost * 5
+      end
+    else
+      return 0
+    end
+  end
 end
 
 # ########### GAME START ########### #
 
 if __FILE__ == $PROGRAM_NAME
-  machine = Machine.new(222, 22)
+  machine = Machine.new(2222, 50)
   player = Player.new(100)
   machine.run
 end
--- spec/fruit_machine_spec.rb
+++ spec/fruit_machine_spec.rb
@@ -36,15 +36,57 @@ describe Machine do
           ).to be false
   end
 
-  it 'can check if two adjacent slots match' do
-    machine = Machine.new(2222, 50)
-    expect(machine.adjacent_win?(['black', 'white', 'green', 'yellow'])
-          ).to be false
-    expect(machine.adjacent_win?(['yellow', 'white', 'green', 'white'])
-          ).to be false
-    expect(machine.adjacent_win?(['white', 'green', 'green', 'yellow'])
-          ).to be true
-    expect(machine.adjacent_win?(['green', 'yellow', 'white', 'white'])
-          ).to be true
+  context 'with a jackpot win condition' do
+    it 'can calculate the jackpot when the bank is even' do
+      machine = Machine.new(2222, 50)
+      result = ['black', 'white', 'green', 'yellow']
+      expect(machine.unique_set_win?(result)).to be true
+      expect(machine.determine_prize(result)).to eq 1111
+      expect(machine.machine_bank).to eq 1111
+    end
+
+    it 'can calculate the jackpot when the bank is odd' do
+      machine = Machine.new(1111, 50)
+      result = ['black', 'white', 'green', 'yellow']
+      expect(machine.unique_set_win?(result)).to be true
+      expect(machine.determine_prize(result)).to be 555
+      expect(machine.machine_bank).to eq 556
+    end
+  end
+
+  context 'with an adjacent win condition' do
+    it 'can check if two adjacent slots match' do
+      machine = Machine.new(2222, 50)
+      expect(machine.adjacent_win?(['black', 'white', 'green', 'yellow'])
+            ).to be false
+      expect(machine.adjacent_win?(['yellow', 'white', 'green', 'white'])
+            ).to be false
+      expect(machine.adjacent_win?(['white', 'green', 'green', 'yellow'])
+            ).to be true
+      expect(machine.adjacent_win?(['green', 'yellow', 'white', 'white'])
+            ).to be true
+    end
+  end
+
+  context 'winning with a low machine bank' do
+    it 'will credit free plays' do
+      machine = Machine.new(110, 50)
+      result = ['green', 'yellow', 'white', 'white']
+      expect(machine.adjacent_win?(result)).to be true
+      expect(machine.determine_prize(result)).to eq 110
+      expect(machine.machine_bank).to eq 0
+      expect(machine.play_credit).to eq 3
+    end
+  end
+
+  context 'with a losing combination' do
+    it 'will not fufill a win condition or give a prize' do
+      machine = Machine.new(2222, 50)
+      result = ['black', 'green', 'black', 'green']
+      expect(machine.unique_set_win?(result)).to be false
+      expect(machine.adjacent_win?(result)).to be false
+      expect(machine.determine_prize(result)).to eq 0
+      expect(machine.machine_bank).to eq 2222
+    end
   end
 end
